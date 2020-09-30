ebdaf5ca53819e2bf12ceb61f11a9f26a4f3e978
alexey.orlov <orel10181@gmail.com>
2020-09-30 17:28:04 +0300

    feat(StepPayload): added FieldResolver widget

--- src/step/resolver.ts
+++ src/step/resolver.ts
@@ -1,5 +1,9 @@
-import { Arg, Query, Resolver } from 'type-graphql';
+import { Arg, Authorized, FieldResolver, Query, Resolver, Root } from 'type-graphql';
 import { Inject } from 'typedi';
+
+import { Roles } from '../roles/roles';
+import { WidgetPayload } from '../widget/payload';
+import { WidgetService } from '../widget/service';
 
 import { StepPayload } from './payload';
 import { StepService } from './service';
@@ -9,7 +13,18 @@ export class StepResolver {
   @Inject()
   private readonly stepService: StepService;
 
+  @Inject()
+  private readonly widgetService: WidgetService;
 
+
+  @Authorized(Roles.ADMIN, Roles.USER)
+  @FieldResolver(() => WidgetPayload)
+  async widget(@Root() step: StepPayload): Promise<WidgetPayload> {
+    const widget = await this.widgetService.findById(step.widgetId)
+    return new WidgetPayload(widget)
+  }
+
+  @Authorized(Roles.ADMIN, Roles.USER)
   @Query(() => StepPayload)
   async step(@Arg("id") id: number): Promise<StepPayload> {
     const step = await this.stepService.findById(id)
