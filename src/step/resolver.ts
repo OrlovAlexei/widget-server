ceb5dd5652c5353e35f9ff3846521783df41b486
alexey.orlov <orel10181@gmail.com>
2020-09-30 17:18:14 +0300

    feat(StepResolver): added Query step

--- src/step/resolver.ts
+++ src/step/resolver.ts
@@ -1,6 +1,18 @@
-import { Resolver } from 'type-graphql';
+import { Arg, Query, Resolver } from 'type-graphql';
+import { Inject } from 'typedi';
 
 import { StepPayload } from './payload';
+import { StepService } from './service';
 
 @Resolver(StepPayload)
-export class StepResolver { }
+export class StepResolver {
+  @Inject()
+  private readonly stepService: StepService;
+
+
+  @Query(() => StepPayload)
+  async step(@Arg("id") id: number): Promise<StepPayload> {
+    const step = await this.stepService.findById(id)
+    return new StepPayload(step)
+  }
+}
